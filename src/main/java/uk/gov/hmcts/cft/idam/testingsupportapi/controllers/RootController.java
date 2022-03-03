6f71d2aec76dfd404ec096714cab747c6eb8f125
James Burke (IDAM) <james.burke@hmcts.net>
2022-03-03 14:46:28 +0000

    initial services and app insight logging

--- src/main/java/uk/gov/hmcts/cft/idam/testingsupportapi/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/cft/idam/testingsupportapi/controllers/RootController.java
@@ -1,8 +1,17 @@
 package uk.gov.hmcts.cft.idam.testingsupportapi.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.cft.idam.api.v2.common.model.ActivatedUserRequest;
+import uk.gov.hmcts.cft.idam.api.v2.common.model.User;
+import uk.gov.service.notify.Notification;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -15,5 +24,27 @@ public class RootController {
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
         return ok("Welcome to idam-testing-support-api application!");
+    }
+
+    @PostMapping("/test/idam/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody ActivatedUserRequest testUser) {
+        return testUser.getUser();
+    }
+
+    @DeleteMapping("/test/idam/users/{userId}")
+    public User deleteUser(@PathVariable String userId) {
+        return null;
+    }
+
+    @GetMapping("/test/idam/notifications/latest/{emailAddress}")
+    public Notification getLatestNotification(@PathVariable String emailAddress) {
+        return null;
+    }
+
+    @PostMapping("/test/idam/burner/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createBurnerUser(@RequestBody User testUser) {
+        return testUser;
     }
 }
