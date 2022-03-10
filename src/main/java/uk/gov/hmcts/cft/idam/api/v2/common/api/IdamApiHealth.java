1c29e342daf15d19a821d95f073ceb1130f92b7a
James Burke (IDAM) <james.burke@hmcts.net>
2022-03-10 17:16:01 +0000

    add health and info endpoints

---
+++ src/main/java/uk/gov/hmcts/cft/idam/api/v2/common/api/IdamApiHealth.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.cft.idam.api.v2.common.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name= "idamapihealth", url = "${idam.api.url}")
+public interface IdamApiHealth extends HealthApi {
+
+}
