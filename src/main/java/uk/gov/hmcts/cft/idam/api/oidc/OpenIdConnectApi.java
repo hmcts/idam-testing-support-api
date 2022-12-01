085bf96c2560041b71ade3b6494d0ab6f03bad84
James Burke (IDAM) <james.burke@hmcts.net>
2022-12-01 15:38:09 +0000

    password grant

--- src/main/java/uk/gov/hmcts/cft/idam/api/oidc/OpenIdConnectApi.java
+++ src/main/java/uk/gov/hmcts/cft/idam/api/oidc/OpenIdConnectApi.java
@@ -1,10 +1,32 @@
 package uk.gov.hmcts.cft.idam.api.oidc;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @FeignClient(name = "openidconnectapi", url = "${idam.api.url}")
 public interface OpenIdConnectApi {
-    
-    void createToken(String grantType, String username, String userSecret, String clientId, String clientSecret, String scopes);
+
+    String PASSWORD_GRANT = "password";
+    String ACCESS_TOKEN = "access_token";
+
+    default String passwordGrant(String username,
+                         String userSecret,
+                         String clientId,
+                         String clientSecret,
+                         String scope) {
+        return createToken(PASSWORD_GRANT, username, userSecret, clientId, clientSecret, scope).get(ACCESS_TOKEN);
+    }
+
+    @PostMapping(value = "/o/token", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    Map<String, String> createToken(@RequestParam("grant_type") String grantType,
+                                    @RequestParam("username") String username,
+                                    @RequestParam("password") String userSecret,
+                                    @RequestParam("client_id") String clientId,
+                                    @RequestParam("client_secret") String clientSecret,
+                                    @RequestParam("scope") String scope);
 
 }
