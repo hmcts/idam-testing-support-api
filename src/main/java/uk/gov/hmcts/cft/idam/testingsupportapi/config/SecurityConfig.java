7535f1891ed6188d691c9e6f42a4fe19ac138902
Nikola Naydenov <nikolanaydenov@Nikolas-MacBook-Pro.local>
2023-08-14 11:29:27 +0100

    Try this

--- src/main/java/uk/gov/hmcts/cft/idam/testingsupportapi/config/SecurityConfig.java
+++ src/main/java/uk/gov/hmcts/cft/idam/testingsupportapi/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.cft.idam.testingsupportapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,7 +10,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(proxyTargetClass = true)
 public class SecurityConfig {
 
     @Bean
