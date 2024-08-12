68d82660dd8bbb3b95e3596f4a12173e92f83dc3
James Burke (IDAM) <james.burke@hmcts.net>
2024-08-12 14:35:39 +0100

    improve coverage

--- src/test/java/uk/gov/hmcts/cft/idam/testingsupportapi/receiver/CleanupReceiverTest.java
+++ src/test/java/uk/gov/hmcts/cft/idam/testingsupportapi/receiver/CleanupReceiverTest.java
@@ -15,7 +15,6 @@ import uk.gov.hmcts.cft.idam.testingsupportapi.service.AdminService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
