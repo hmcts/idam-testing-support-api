7d4c01bce9b4b64f41eeacddc94052a70a116527
Muhammad <muhammad.umerji@hmcts.net>
2024-09-27 16:45:24 +0100

    Pinning Azuread to version 2.53.1 in order to fix azuread issue

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,16 @@
 terraform {
-  backend "azurerm" {}
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.53.1"
+    }
   }
 }
+
 
 provider "azurerm" {
   features {}
