b91d51fc19af1016a1588b2410d3a95a5e4d8603
Muhammad <muhammad.umerji@hmcts.net>
2024-10-23 13:12:29 +0100

    Adding testing-support's secrects back

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,3 +20,38 @@ provider "azurerm" {
   alias = "cft_vnet"
 }
 
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  count        = local.instance_count
+  name         = "${local.default_name}-POSTGRES-USER"
+  value        = module.idam-testing-support-api-db-v14[0].username
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  count        = local.instance_count
+  name         = "${local.default_name}-POSTGRES-PASS"
+  value        = module.idam-testing-support-api-db-v14[0].password
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  count        = local.instance_count
+  name         = "${local.default_name}-POSTGRES-HOST"
+  value        = module.idam-testing-support-api-db-v14[0].fqdn
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+# These two are not exported by the v14 module, but used by the pods
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  count        = local.instance_count
+  name         = "${local.default_name}-POSTGRES-PORT"
+  value        = "5432"
+  key_vault_id = data.azurerm_key_vault.default.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  count        = local.instance_count
+  name         = "${local.default_name}-POSTGRES-DATABASE"
+  value        = var.database_name
+  key_vault_id = data.azurerm_key_vault.default.id
+}
