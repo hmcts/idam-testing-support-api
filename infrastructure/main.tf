af8d11d6e19b6dd5f1aa77866e86e63c59f7d791
Muhammad <muhammad.umerji@hmcts.net>
2024-11-04 11:07:35 +0000

    Removing the main.tf infra within sandbox branch

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,13 +13,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  subscription_id            = local.cft_vnet[local.env].subscription
-  skip_provider_registration = "true"
-  features {}
-  alias = "cft_vnet"
-}
-
 locals {
   default_name   = "${var.product}-${var.component}"
   vault_name     = "${var.product}-${var.env}"
@@ -50,74 +43,4 @@ locals {
       subscription = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
     }
   }
-}
-
-module "idam-testing-support-api-db-v14" {
-  count = local.instance_count
-
-  providers = {
-    azurerm.postgres_network = azurerm.cft_vnet
-  }
-
-  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  env    = var.env
-
-  product       = var.product
-  component     = var.component
-  business_area = "cft"
-  common_tags   = var.common_tags
-  name          = "idam-testing-support-api-v14"
-
-  subnet_suffix = "expanded"
-
-  pgsql_databases = [
-    {
-      name : var.database_name
-    }
-  ]
-
-  pgsql_version = "14"
-
-  admin_user_object_id = var.jenkins_AAD_objectId
-}
-
-data "azurerm_key_vault" "default" {
-  name                = local.vault_name
-  resource_group_name = "${var.product}-${var.env}"
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  count        = local.instance_count
-  name         = "${local.default_name}-POSTGRES-USER"
-  value        = module.idam-testing-support-api-db-v14[0].username
-  key_vault_id = data.azurerm_key_vault.default.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  count        = local.instance_count
-  name         = "${local.default_name}-POSTGRES-PASS"
-  value        = module.idam-testing-support-api-db-v14[0].password
-  key_vault_id = data.azurerm_key_vault.default.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  count        = local.instance_count
-  name         = "${local.default_name}-POSTGRES-HOST"
-  value        = module.idam-testing-support-api-db-v14[0].fqdn
-  key_vault_id = data.azurerm_key_vault.default.id
-}
-
-# These two are not exported by the v14 module, but used by the pods
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  count        = local.instance_count
-  name         = "${local.default_name}-POSTGRES-PORT"
-  value        = "5432"
-  key_vault_id = data.azurerm_key_vault.default.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  count        = local.instance_count
-  name         = "${local.default_name}-POSTGRES-DATABASE"
-  value        = var.database_name
-  key_vault_id = data.azurerm_key_vault.default.id
 }
