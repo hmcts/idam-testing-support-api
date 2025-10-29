1f4f6758b33b5111db208c054712c3bedc42ef64
James Burke (IDAM) <james.burke@hmcts.net>
2025-10-29 13:57:28 +0000

    test properties

--- src/test/java/uk/gov/hmcts/cft/idam/testingsupportapi/properties/BurnerUserCreationPropertiesTest.java
+++ src/test/java/uk/gov/hmcts/cft/idam/testingsupportapi/properties/BurnerUserCreationPropertiesTest.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.cft.idam.testingsupportapi.properties;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-=import java.util.List;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
